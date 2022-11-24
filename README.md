9d9cf8162271930db93ef87899c09a3a6bc51cf6
Jan <enderjanyt@gmail.com>
2022-11-24 12:44:34 +0100

    Erster Beitrag

---
+++ README.md
@@ -0,0 +1,4 @@
+# FAUlang
+
+Ganz wichtig: keine Plagiate!
+Deswegen auch keine Dependencies. Wir können selbst eh alles besser!
---
+++ src/Haupt.java
@@ -0,0 +1,5 @@
+public class Haupt {
+  public static void main(String[] args) {
+    System.out.println("KEINE PLAGIATE!");
+  }
+}

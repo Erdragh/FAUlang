e2c4cdb1b4f9fcc88bb133be7d993426e382cd27
Jan Bayer <enderjanyt@gmail.com>
2022-11-26 19:25:37 +0100

    Füge faulen Methoden hinzu

--- src/com/github/erdragh/faulang/Haupt.java
+++ src/com/github/erdragh/faulang/Haupt.java
@@ -1,7 +1,25 @@
 package com.github.erdragh.faulang;
 
 public class Haupt {
+
+  public static final String NAME = "faul";
+
   public static void main(String[] parameter) {
+    if (parameter.length > 1) {
+      System.out.println("Benutzen: " + NAME + " [Skript]");
+      System.exit(64);
+    } else if (parameter.length == 1) {
+      dateiFaulen(parameter[0]);
+    } else {
+      eingabeFaulen();
+    }
+  }
+
+  private static void dateiFaulen(String skriptPfad) {
+
+  }
+
+  private static void eingabeFaulen() {
 
   }
 }
